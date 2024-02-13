2ddd89aafc3d3763f70c6693fb1702983447b9f1
Frankie Dick <fhd96@hotmail.co.uk>
2024-02-13 19:29:45 +0000

    Created Checklist & Task models, migrations, controllers and Routes for 'getAll' methods

---
+++ app/Http/Controllers/ChecklistController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ChecklistController extends Controller
+{
+    public function getAllChecklists()
+    {
+        
+    }
+}
